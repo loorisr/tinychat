8e75b80906575d52039f91816fda4899e6172b37
Loris <loris.rion@gmail.com>
2025-02-24 16:43:17 +0100

    ask searching and scraping capabilities

--- README.md
+++ README.md
@@ -2,5 +2,23 @@ Very simple LLM chat frontend with Fastapi
 
 ![image](https://github.com/user-attachments/assets/9f8de766-4732-424c-997f-c4ddac95546c)
 
+I wanted a very lightweight and simple chat interface to try several LLM.
 
-*Streamlit image was 1.1 Go, so I've switched to Fastapi : image is now 190 mo!*
+There are several projects but non of them are very lightweight (hundreds of Mb to Gb):
+* https://github.com/open-webui/open-webui
+* https://github.com/jasonacox/TinyLLM/tree/main/chatbot
+* https://github.com/fmaclen/hollama
+* ...
+
+So I decided to build a very simple chat interface. The docker image size is 161 mo.
+
+It can search on the internet and scrape pages thanks to [Firecrawl](https://www.firecrawl.dev/) (which is self-hostable).
+
+Thanks to [LiteLLM](https://github.com/BerriAI/litellm), you can configure as many models as you want inside `models.yaml`.
+
+````yaml
+mistral-large:
+  api_key: "your-api-key"
+  endpoint: "https://api.mistral.ai/v1"
+  model_name: "mistral/mistral-large-latest"
+````
--- fastapi/app/app.py
+++ fastapi/app/app.py
@@ -3,10 +3,26 @@ from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 import litellm
 import asyncio
+import json
 import yaml
+import time
 import os
 from pathlib import Path
-import time
+from datetime import datetime
+from firecrawl.firecrawl import FirecrawlApp
+
+FIRECRAWL_API_KEY = os.getenv("FIRECRAWL_API_KEY", "my-api-key")
+FIRECRAWL_API_URL = os.getenv("FIRECRAWL_API_URL", "https://api.firecrawl.dev")
+
+firecrawl_client = FirecrawlApp(api_key=FIRECRAWL_API_KEY, api_url=FIRECRAWL_API_URL)
+
+system_prompt = f"""
+You are a helpful assistant.
+Your name is Tiny Chat. Today is {datetime.now().isoformat()}
+You can use Markdown.
+You can make search on internet using the command: "/search $QUERY$". Return a message with only this command.
+You can read a page on internet using the command: "/scrape $page_adresse$". Return a message with only this command. One per line. Try to access only to the necessary pages.
+"""
 
 app = FastAPI()
 
@@ -40,11 +56,17 @@ async def read_root(request: Request):
 
     return content
 
+async def send_LLM_message(message_session, llm_config):
+    return True
+
+
 
 @app.websocket("/chat")
 async def chat_endpoint(websocket: WebSocket):
     await websocket.accept()
     try:
+        message_session = []
+        message_session.append({"role": "system", "content": system_prompt})
         while True:
             data = await websocket.receive_json()
             user_message = data.get("message", "")
@@ -61,30 +83,100 @@ async def chat_endpoint(websocket: WebSocket):
                 continue
 
             llm_config = LLM_CONFIGS[llm_name]
+            message_session.append( {"role": "user", "content": user_message})
 
-            start_time = time.time()
+            assistant_message = ""
+
+            #start_time = time.time()
             user_ip = websocket.client.host
             print(f"Model {llm_config['model_name']}, message {user_message}, IP: {user_ip}")
             response = await litellm.acompletion(
                 model=llm_config["model_name"],
-                messages=[{"content": user_message, "role": "user"}],
+                #messages=[{"role": "system", "content": system_prompt}, {"role": "user", "content": user_message}],
+                messages=message_session,
                 temperature=0.7,
                 api_key=llm_config.get("api_key"),
                 api_base=llm_config.get("endpoint"),
                 stream=True,
+                stream_options={"include_usage": True},
             )
 
             total_tokens = 0
             async for chunk in response:
                 if chunk.choices[0].delta.content is not None:
                     content_chunk = chunk.choices[0].delta.content
+                    assistant_message += content_chunk
                     await websocket.send_text(content_chunk)
                     if "usage" in chunk:
                         total_tokens = chunk.usage.total_tokens
 
-            end_time = time.time()
-            duration = end_time - start_time
-            tokens_per_second = total_tokens / duration if duration > 0 else 0
+            message_session.append( {"role": "assistant", "content": assistant_message})
+
+            if "/search" in assistant_message:
+                query = assistant_message[len("/search "):]
+                assistant_message = ""
+                print(f"Searching for {query}")
+                await websocket.send_text(f"<br />Searching for *{query}* <br/><br/>")
+
+                search_results = firecrawl_client.search(query)
+                #print(json.dumps(search_results['data']))
+                
+                message_session.append( {"role": "user", "content": json.dumps(search_results['data'])})
+                response = await litellm.acompletion(
+                    model=llm_config["model_name"],
+                    #messages=[{"role": "system", "content": system_prompt}, {"role": "user", "content": user_message}],
+                    messages=message_session,
+                    temperature=0.7,
+                    api_key=llm_config.get("api_key"),
+                    api_base=llm_config.get("endpoint"),
+                    stream=True,
+                    stream_options={"include_usage": True},
+                )
+
+                async for chunk in response:
+                    if chunk.choices[0].delta.content is not None:
+                        content_chunk = chunk.choices[0].delta.content
+                        assistant_message += content_chunk
+                        await websocket.send_text(content_chunk)
+
+                message_session.append( {"role": "assistant", "content": assistant_message})
+
+            
+            if "/scrape" in assistant_message:
+                lines = assistant_message.split()
+                assistant_message = ""
+                #url = lines[len("/scrape "):]
+                urls = [line for line in lines if line != "/scrape"]
+                print(f"Scraping pages {urls}")
+                await websocket.send_text(f"<br />Scraping pages {urls}<br />")
+
+                scrape_results = firecrawl_client.batch_scrape_urls(urls, params={"formats": ["markdown"]})
+                for page in scrape_results["data"]:
+                    del page["metadata"]
+                
+                message_session.append( {"role": "user", "content": json.dumps(scrape_results["data"])})
+                response = await litellm.acompletion(
+                    model=llm_config["model_name"],
+                    #messages=[{"role": "system", "content": system_prompt}, {"role": "user", "content": user_message}],
+                    messages=message_session,
+                    temperature=0.7,
+                    api_key=llm_config.get("api_key"),
+                    api_base=llm_config.get("endpoint"),
+                    stream=True,
+                    stream_options={"include_usage": True},
+                )
+
+                async for chunk in response:
+                    if chunk.choices[0].delta.content is not None:
+                        content_chunk = chunk.choices[0].delta.content
+                        assistant_message += content_chunk
+                        await websocket.send_text(content_chunk)
+
+                message_session.append( {"role": "assistant", "content": assistant_message})
+    
+            #end_time = time.time()
+            #duration = end_time - start_time
+            #tokens_per_second = total_tokens / duration if duration > 0 else 0
             #await websocket.send_json(
             #    {"total_tokens": total_tokens, "tokens_per_second": f"{tokens_per_second:.2f}"}
             #)  # send as json
